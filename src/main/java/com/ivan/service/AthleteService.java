fe9c08ac9c4b703145d2cdcec2c05926832d6ea1
ssssssergeenkov <itproger181920@gmail.com>
2024-04-12 04:36:26 +0300

    new service

---
+++ src/main/java/com/ivan/service/AthleteService.java
@@ -0,0 +1,10 @@
+package com.ivan.service;
+
+import com.ivan.model.Athlete;
+
+import java.util.List;
+
+public interface AthleteService {
+
+    List<Athlete> showAllAthlete();
+}
---
+++ src/main/java/com/ivan/service/impl/AthleteServiceImpl.java
@@ -0,0 +1,18 @@
+package com.ivan.service.impl;
+
+import com.ivan.dao.AthleteDao;
+import com.ivan.model.Athlete;
+import com.ivan.service.AthleteService;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+public class AthleteServiceImpl implements AthleteService {
+
+    private final AthleteDao athleteDao;
+    @Override
+    public List<Athlete> showAllAthlete() {
+        return athleteDao.findAll();
+    }
+}
