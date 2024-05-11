7114029a6399a85065b7e43d5247efffb0aaeb02
ssssssergeenkov <itproger181920@gmail.com>
2024-05-11 03:43:54 +0300

    added tests for controller

---
+++ src/test/java/com/ivan/controller/ClientControllerTest.java
@@ -0,0 +1,35 @@
+package com.ivan.controller;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith(MockitoExtension.class)
+class ClientControllerTest {
+
+    @Test
+    void showAllTrainingTypes() {
+    }
+
+    @Test
+    void showTrainingByDate() {
+    }
+
+    @Test
+    void showTrainingBySetsAmount() {
+    }
+
+    @Test
+    void addTraining() {
+    }
+
+    @Test
+    void deleteTraining() {
+    }
+
+    @Test
+    void editTraining() {
+    }
+}
